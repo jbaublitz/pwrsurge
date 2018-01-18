cfe8c515a2b06e778d61f909448e6f3c93c42cb5
John Baublitz <john.m.baublitz@gmail.com>
2018-01-18 18:59:44 -0500

    Fix errors that did not pop up locally because of Cargo.lock stuff

--- src/netlink.rs
+++ src/netlink.rs
@@ -3,16 +3,16 @@ use acpi::AcpiEvent;
 use nl::socket::NlSocket;
 use nl::nlhdr::{NlHdr,NlAttrHdr};
 use nl::genlhdr::{GenlHdr};
-use nl::ffi::{NlFamily,NlType,NlFlags,NlaType,GenlType,GenlCmds,AttrTypeMcast};
+use nl::ffi::{NlFamily,Nlmsg,NlFlags,CtrlAttr,GenlId,CtrlCmd,CtrlAttrMcastGrp};
 use nl::err::{NlError};
 
 const ACPI_FAMILY_NAME: &'static str = "acpi_event";
 
 pub fn acpi_listen() -> Result<(), NlError> {
     let id = resolve_acpi_family_id()?;
-    let mut s = NlSocket::connect(NlFamily::NlGeneric, None, Some(1 << (id - 1)))?;
+    let mut s = NlSocket::connect(NlFamily::Generic, None, Some(1 << (id - 1)))?;
     loop {
-        let msg = s.recvmsg::<NlType, GenlHdr>(Some(4096), 0)?;
+        let msg = s.recvmsg::<Nlmsg, GenlHdr>(Some(4096), 0)?;
         let mut handle = msg.nl_payload.get_attr_handle::<u16>();
         let acpi_event = handle.get_payload_as::<AcpiEvent>(1);
         println!("{:?}", acpi_event);
@@ -20,19 +20,19 @@ pub fn acpi_listen() -> Result<(), NlError> {
 }
 
 pub fn resolve_acpi_family_id() -> Result<u32, NlError> {
-    let mut s = NlSocket::new(NlFamily::NlGeneric)?;
-    let attrs = vec![NlAttrHdr::new_str_payload(None, NlaType::AttrFamilyName,
+    let mut s = NlSocket::new(NlFamily::Generic)?;
+    let attrs = vec![NlAttrHdr::new_str_payload(None, CtrlAttr::FamilyName,
                      ACPI_FAMILY_NAME)];
-    let genl_hdr = GenlHdr::new(GenlCmds::CmdGetfamily, 2, attrs)?;
-    let msg = NlHdr::<GenlType, GenlHdr>::new(None, GenlType::IdCtrl,
-                                              vec![NlFlags::NlRequest, NlFlags::NlAck],
+    let genl_hdr = GenlHdr::new(CtrlCmd::Getfamily, 2, attrs)?;
+    let msg = NlHdr::<GenlId, GenlHdr>::new(None, GenlId::Ctrl,
+                                              vec![NlFlags::Request, NlFlags::Ack],
                                               None, None, genl_hdr);
     s.sendmsg(msg, 0)?;
-    let resp = s.recvmsg::<NlType, GenlHdr>(Some(4096), 0)?;
-    let mut resp_handle = resp.nl_payload.get_attr_handle::<NlaType>();
-    let mut mcastgroups = resp_handle.get_nested_attributes::<u16>(NlaType::AttrMcastGroups)?;
-    let mut mcastgroup = mcastgroups.get_nested_attributes::<AttrTypeMcast>(1u16)?;
-    let id = mcastgroup.get_payload_as::<u32>(AttrTypeMcast::GrpId)?;
+    let resp = s.recvmsg::<Nlmsg, GenlHdr>(Some(4096), 0)?;
+    let mut resp_handle = resp.nl_payload.get_attr_handle::<CtrlAttr>();
+    let mut mcastgroups = resp_handle.get_nested_attributes::<u16>(CtrlAttr::McastGroups)?;
+    let mut mcastgroup = mcastgroups.get_nested_attributes::<CtrlAttrMcastGrp>(1u16)?;
+    let id = mcastgroup.get_payload_as::<u32>(CtrlAttrMcastGrp::Id)?;
     Ok(id)
 }
 
